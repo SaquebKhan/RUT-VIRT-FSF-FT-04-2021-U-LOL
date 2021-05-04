b21bd200533a44a3ad6c6664d8a9724f405423db
SaquebKhan <82435053+SaquebKhan@users.noreply.github.com>
2021-05-04 01:57:27 -0400

    class activities

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,8 @@
 // Write Your JavaScript Code Here
+var personName = "Saqueb";
+var pets = 0
+var funFact = "I love video games";
+console.log("My name is " + personName)
+console.log("Ihave " + pets)
+console.log(funFact);
+
